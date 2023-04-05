897822683ef94c4f4cb0126caaf1093f29685c10
BiswaDo <biswadipsaha503@gmail.com>
2023-04-05 13:36:03 +0530

    c

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 
 
 mongoose.set('strictQuery', false);
-mongoose.connect('"'+process.env.MONGO_URI+'"',{useNewUrlParser:true});
+mongoose.connect('"'+process.env.mongo+'"',{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
     email : String,
